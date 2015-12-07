517eb67103c0194efddc995caf3523f6d70aeff3
Meghan Green <meghanleighgreen@gmail.com>
2015-12-06 22:41:36 -0400

    aaaand part two done

--- day6.py
+++ day6.py
@@ -25,15 +25,24 @@ def count(mat):
     return tally
 
 
-def switch(s, state):
-    if (s[:3] == 'tog' or s[:6] == 'turnof') and state == 1:
-        state = 0
-    elif (s[:3] == 'tog' or s[:6] == 'turnon') and state == 0:
-        state = 1
+def switch(s, state, r):
+    if r == 0:
+        if (s[:3] == 'tog' or s[:6] == 'turnof') and state == 1:
+            state = 0
+        elif (s[:3] == 'tog' or s[:6] == 'turnon') and state == 0:
+            state = 1
+    elif r == 1:
+        if s[:3] == 'tog':
+            state += 2
+        elif s[:6] == 'turnof':
+            if state > 0:
+                state -= 1
+        elif s[:6] == 'turnon':
+            state += 1
     return state
 
 
-def main(s):
+def main(s,r):
     l = s.split('\n')
     grid = blankgrid(1000,1000)
     for inst in l:
@@ -41,12 +50,14 @@ def main(s):
         instlist = inst.split()
         p1 = instlist[1].split(',')
         p2 = instlist[3].split(',')
-        (x1,y1) = p1
-        (x2,y2) = p2
+        x1 = int(p1[0])
+        x2 = int(p2[0])
+        y1 = int(p1[1])
+        y2 = int(p2[1])
         for posx in range(x1,x2+1):
             for posy in range(y1,y2+1):
-                grid[posx][posy] = switch(instlist[0],grid[posx][posy])
+                grid[posx][posy] = switch(instlist[0],grid[posx][posy],r)
     sous = count(grid)
     return sous
 
-print main(inp)
+print "Day 6: \nPart 1: {0} \nPart 2: {1}".format(main(inp,0),main(inp,1))
