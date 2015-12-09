e5851141a46801f2d081b6ed548a94ab98ff83eb
Meghan Green <meghanleighgreen@gmail.com>
2015-12-09 17:12:47 -0400

    Second attempt

--- day7.py
+++ day7.py
@@ -9,19 +9,18 @@ def bitwise(s):
     signals = {}
     l = s.split('\n')
     for i in l:
-        inst = i.split(' -> ') # [instructions, output]
-        piece = inst[0].split() # [split by instruction pieces]
-        out = inst[1]
-        if piece[0] == 'NOT':
-            signals[out] = ~ signals[piece[1]]
-        elif piece[1] == 'AND':
-            signals[out] = signals[piece[0]] & signals[piece[2]]
-        elif piece[1] == 'OR':
-            signals[out] = signals[piece[0]] | signals[piece[2]]
-        elif piece[1] == 'RSHIFT':
-            signals[out] = signals[piece[0]] >> signals[piece[2]]
-        elif piece[1] == 'LSHIFT':
-            signals[out] = signals[piece[0]] << signals[piece[2]]
-    return signals['a']
+        inst = i.split() # [instructions, output]
+        out = inst[len(inst)-1]
+        if inst[0] == 'NOT':
+            signals[out] = ~ signals[inst[1]]
+        elif inst[1] == 'AND':
+            signals[out] = signals[inst[0]] & signals[inst[2]]
+        elif inst[1] == 'OR':
+            signals[out] = signals[inst[0]] | signals[inst[2]]
+        elif inst[1] == 'RSHIFT':
+            signals[out] = signals[inst[0]] >> signals[inst[2]]
+        elif inst[1] == 'LSHIFT':
+            signals[out] = signals[inst[0]] << signals[inst[2]]
+    return signals
 
 print bitwise(inp)
