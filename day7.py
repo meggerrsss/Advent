5bd86bd895624c69fb90fa95a8f0516f41e396e2
Meghan <meghanleighgreen@gmail.com>
2015-12-14 09:42:03 -0400

    One week late but part 1 works

--- day7.py
+++ day7.py
@@ -5,61 +5,17 @@ inp = f.read()
 f.close()
 
 
-def tangle(i,d):
-    if i[1] == '<-':
-        if isinstance(i[0], (int, long)):
-            want = [i[0]]
-        else:
-            want = [d[i[0]]]
-    elif i[0] == 'NOT':
-        want = [~d[i[1]]]
-    elif i[1] == 'AND':
-        want = [d[i[0]] & d[i[2]]]
-    elif i[1] == 'OR':
-        want = [d[i[0]] | d[i[2]]]
-    elif i[1] == 'RSHIFT':
-        want = [d[i[0]] >> d[i[2]]]
-    else:# i[1] == 'LSHIFT':
-        want = [d[i[0]] << d[i[2]]]
-    return want
-
-
 def findinputs(s):
     i = s.split()
     a = i[2]
-    if len(i)<4:
+    if i[3] == '->':
         b = i[2]
-    elif i[2] == 'NOT':
+    elif i[4] == '->':
         a = i[3]
         b = i[3]
-    elif len(i)>4:
+    elif i[5] == '->':
         b = i[4]
-    else:
-        b=a
     return [a,b]
-
-
-def make(s, d):
-    l = s.split('\n')
-    length = len(l)
-    if length>0:
-        newst = ''
-        for i in l:
-            inst = i.split()
-            #print inst
-            if not inst == []:
-                out = inst[len(inst)-1]
-                a = findinputs(i)[0]
-                b = findinputs(i)[1]
-                if not (d.get(a) and d.get(b)):
-                    newst += ' \n ' +i
-                else:
-                    d[out] = tangle(inst, d)
-        #print newst
-        return make(newst,d)
-    else:
-        return d
-
 
 
 def sortthething(s):
@@ -73,36 +29,38 @@ def sortthething(s):
         new += thing+'\n'
     return new
 
-
 newinp = sortthething(inp)
+
 
 def net(s):
     d = {}
     l = s.split('\n')
-    newl = l[1:]
+    newl = l[1:]+[l[0]]
     for i in newl:
-        if not i == []:
-            inst = i.split()
+        inst = i.split()
+        if not inst == []:
             out = inst[0]
             aaa = findinputs(i)[0]
             bbb = findinputs(i)[1]
             if aaa.isdigit():
-                d[aaa] = aaa
+                d[aaa] = int(aaa)
             if bbb.isdigit():
-                d[bbb] = bbb
-            if len(inst) == 3:
-                d[out] = d[aaa]
-            elif inst[2] == "NOT":
+                d[bbb] = int(bbb)
+            if len(inst) == 4:
+                if aaa.isdigit():
+                    d[out] = int(aaa)
+                else:
+                    d[out] = d[aaa]
+            if inst[2] == "NOT":
                 d[out] = ~ d[aaa]
-            elif inst[3] == "AND":
-                d[out] = d[aaa] & d[bbb]
-            elif inst[3] == "OR":
-                d[out] = d[aaa] | d[bbb]
-            elif inst[3] == "LSHIPT":
-                d[out] = d[aaa] << d[bbb]
-            elif inst[3] == "RSHIFT":
-                d[out] = d[aaa] >> d[bbb]
-        print d
+            if inst[3] == "AND":
+                d[out] = d[aaa] & d[bbb] #changing to two cases, one with isdigit and one with two characters
+            if inst[3] == "OR":
+                d[out] = d[aaa] | d[bbb] #also also change
+            if inst[3] == "LSHIFT":
+                d[out] = d[aaa] << int(bbb)
+            if inst[3] == "RSHIFT":
+                d[out] = d[aaa] >> int(bbb)
     return d
 
 print net(newinp)
