a63232e7181043c1935d4a41fc3c1e6bbd8cf869
Meghan <meghanleighgreen@gmail.com>
2015-12-18 10:47:46 -0500

    Cleaner and added a second function that looks shorter but doesn't totally work for part two.

--- day12.py
+++ day12.py
@@ -13,11 +13,6 @@ def collect(s,bad,l):
                     collect(i,bad,l)
                 elif isinstance(i,(int,long)):
                     l.append(i)
-            for i in s.keys():
-                if isinstance(i,dict) or isinstance(i,list):
-                    collect(i,bad,l)
-                elif isinstance(i,(int,long)):
-                    l.append(i)
     elif isinstance(s,list):
         for i in s:
             if isinstance(i,dict) or isinstance(i,list):
@@ -27,4 +22,18 @@ def collect(s,bad,l):
     return l
 
 
-print sum(collect(inp,['None'],[])),sum(collect(inp,'red',[]))
+def collect2(s,bad,l):
+    if isinstance(s,dict):
+        workinglist = [s.values(), s.keys()]
+    elif isinstance(s,list):
+        workinglist = s
+    if bad not in workinglist:
+        for i in workinglist:
+            if isinstance(i,dict) or isinstance(i,list):
+                collect(i,bad,l)
+            elif isinstance(i,(int,long)):
+                l.append(i)
+    return l
+
+
+print "Day 11: \nPart 1: {0} \nPart 2: {1}".format(sum(collect(inp,'None',[])),sum(collect(inp,'red',[])))
