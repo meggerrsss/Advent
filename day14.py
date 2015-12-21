0e10b4f2e61e3f8d9496152c5c1833336c7494db
Meghan <meghanleighgreen@gmail.com>
2015-12-21 15:13:00 -0500

    Part one done, part two.... well...

---
+++ day14.py
@@ -0,0 +1,85 @@
+__author__ = 'Meghan'
+
+f = open('day14.txt', 'r')
+inp = f.read()
+f.close()
+
+
+def interpret(s):
+    l = s.split()
+    name = l[0]
+    speed = int(l[3])
+    golen = int(l[6])
+    restlen = int(l[13])
+    return (name, speed, golen, restlen)
+
+
+def effectiverate(s):
+    stats = interpret(s)
+    dist = stats[1]*stats[2]
+    time = float(stats[2]+stats[3])
+    return dist/time
+
+
+def speedvector(s,t):
+    pos = [0]*t
+    stats = interpret(s)
+    name = stats[0]
+    speed = stats[1]
+    gotime = stats[2]
+    stoptime = stats[3]
+    golist = range(0,gotime)
+    stoplist = range(gotime,stoptime)
+    cycletime = gotime+stoptime
+    for i in range(t):
+        if (i%cycletime) in golist:
+            pos[i]=speed
+    return pos
+
+
+def posvector(v):
+    length = len(v)
+    d = [0]*length
+    d[0] = v[0]
+    for i in range(1,length):
+        d[i] = d[i-1]+v[i]
+    return d
+
+
+def furthest(s,t):
+    reindeer = s.split('\n')
+    poslist = [0]*len(reindeer)
+    nameslist = [0]*len(reindeer)
+    for i in range(len(reindeer)):
+        stats = interpret(reindeer[i])
+        v = speedvector(reindeer[i],t)
+        d = posvector(v)
+        poslist[i] = d[t-1]
+        nameslist[i] = stats[0]
+    return poslist
+
+
+def points(s,t):
+    point = [0]*9
+    for i in range(t):
+        poses = furthest(s,i)
+        far = max(poses)
+        print poses,far
+        for deer in range(len(poses)):
+            if poses[deer] == far:
+                point[deer] += 1
+    return point
+
+
+
+print points(inp,100)
+
+print max(furthest(inp,2503))#,  #points(inp, 2503)
+
+
+
+
+s = "Vixen can fly 18 km/s for 5 seconds, but then must rest for 84 seconds."
+
+
+#print "Day 14: \nPart 1: {0} \nPart 2: {1}".format(a,b)
