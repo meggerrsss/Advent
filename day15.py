c2271ec2d1ab19b896bb0e69c13d503a1469e690
Meghan <meghanleighgreen@gmail.com>
2015-12-22 13:22:41 -0500

    Not quote yet, but getting a number for part two

--- day15.py
+++ day15.py
@@ -32,38 +32,39 @@ def propmatrix(s): #i is each ingredient, j is position of its properties
     return grid
 
 
-def candyscore(matrix, a, b, c, d):
-    cap = a*matrix[0][1] + b*matrix[1][1] + c*matrix[2][1] + d*matrix[3][1]
-    dur = a*matrix[0][2] + b*matrix[1][2] + c*matrix[2][2] + d*matrix[3][2]
-    fla = a*matrix[0][3] + b*matrix[1][3] + c*matrix[2][3] + d*matrix[3][3]
-    tex = a*matrix[0][4] + b*matrix[1][4] + c*matrix[2][4] + d*matrix[3][4]
-    rawp = cap*dur*fla*tex
-    if cap<=0:
-        cap = 0
-    if dur<=0:
-        dur = 0
-    if fla<=0:
-        fla = 0
-    if tex<=0:
-        tex = 0
-    newp = cap*dur*fla*tex
-    return newp
+def candyscore(matrix, a, b, c, d, r):
+    cal = a*matrix[0][5] + b*matrix[1][5] + c*matrix[2][5] + d*matrix[3][5]
+    if r == 2 and not cal == 500:
+        return 0
+    else:
+        cap = a*matrix[0][1] + b*matrix[1][1] + c*matrix[2][1] + d*matrix[3][1]
+        dur = a*matrix[0][2] + b*matrix[1][2] + c*matrix[2][2] + d*matrix[3][2]
+        fla = a*matrix[0][3] + b*matrix[1][3] + c*matrix[2][3] + d*matrix[3][3]
+        tex = a*matrix[0][4] + b*matrix[1][4] + c*matrix[2][4] + d*matrix[3][4]
+        rawp = cap*dur*fla*tex
+        if cap<=0:
+            cap = 0
+        if dur<=0:
+            dur = 0
+        if fla<=0:
+            fla = 0
+        if tex<=0:
+            tex = 0
+        newp = cap*dur*fla*tex
+        return newp
 
 
-def best(s):
+def best(s,r):
     mat = propmatrix(s)
     scores = []
     for a in range(0,100+1):
         for b in range(0,100-a+1):
             for c in range(0,100-a-b+1):
                 for d in range(0,100-a-b-c+1):
-                    score = candyscore(mat, a, b, c, d)
+                    score = candyscore(mat, a, b, c, d, r)
                     if score >0:
                         scores.append(score)
     return scores
 
 
-print max(best(inp))
-
-
-#print "Day 15: \nPart 1: {0} \nPart 2: {1}".format(a,b)
+print "Day 15: \nPart 1: {0} \nPart 2: {1}".format(max(best(inp,1)), max(best(inp,2)))
