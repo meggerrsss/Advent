70f48d9164e400faf70b49c6f43fd74ad31eb9bf
Meghan <meghanleighgreen@gmail.com>
2015-12-22 11:06:53 -0500

    Part one

--- day15.py
+++ day15.py
@@ -32,7 +32,38 @@ def propmatrix(s): #i is each ingredient, j is position of its properties
     return grid
 
 
-print propmatrix(inp)
+def candyscore(matrix, a, b, c, d):
+    cap = a*matrix[0][1] + b*matrix[1][1] + c*matrix[2][1] + d*matrix[3][1]
+    dur = a*matrix[0][2] + b*matrix[1][2] + c*matrix[2][2] + d*matrix[3][2]
+    fla = a*matrix[0][3] + b*matrix[1][3] + c*matrix[2][3] + d*matrix[3][3]
+    tex = a*matrix[0][4] + b*matrix[1][4] + c*matrix[2][4] + d*matrix[3][4]
+    rawp = cap*dur*fla*tex
+    if cap<=0:
+        cap = 0
+    if dur<=0:
+        dur = 0
+    if fla<=0:
+        fla = 0
+    if tex<=0:
+        tex = 0
+    newp = cap*dur*fla*tex
+    return newp
+
+
+def best(s):
+    mat = propmatrix(s)
+    scores = []
+    for a in range(0,100+1):
+        for b in range(0,100-a+1):
+            for c in range(0,100-a-b+1):
+                for d in range(0,100-a-b-c+1):
+                    score = candyscore(mat, a, b, c, d)
+                    if score >0:
+                        scores.append(score)
+    return scores
+
+
+print max(best(inp))
 
 
 #print "Day 15: \nPart 1: {0} \nPart 2: {1}".format(a,b)
