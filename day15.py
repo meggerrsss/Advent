48a12b91903c0c1240bfc408b8490961cba2dc41
Meghan <meghanleighgreen@gmail.com>
2015-12-21 19:02:03 -0500

    Matrix of ingredient properties

--- day15.py
+++ day15.py
@@ -19,4 +19,20 @@ def interpret(s):
     return (ing, capacity, durability, flavour, texture, calories)
 
 
+def blankgrid(n,m): # n is number of rows, m is number of columns, taken from day 6
+    grid = [[0 for j in xrange(m)] for i in xrange(n)]
+    return grid
+
+
+def propmatrix(s): #i is each ingredient, j is position of its properties
+    l = s.split('\n')
+    grid = blankgrid(4,6)
+    for i in range(len(l)):
+        grid[i] = interpret(l[i])
+    return grid
+
+
+print propmatrix(inp)
+
+
 #print "Day 15: \nPart 1: {0} \nPart 2: {1}".format(a,b)
