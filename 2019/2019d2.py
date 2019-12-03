884e27db2496e5511dc25416ac3cbe972dca8914
Meghan <meghanleighgreen@gmail.com>
2019-12-03 03:13:03 +0000

    copying and pasting to and from the online IDE and gitpod broke stuff

--- 2019/2019d2.py
+++ 2019/2019d2.py
@@ -2,6 +2,8 @@
 #input formatting
 inp = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,9,23,27,1,6,27,31,1,31,9,35,2,35,10,39,1,5,39,43,2,43,9,47,1,5,47,51,1,51,5,55,1,55,9,59,2,59,13,63,1,63,9,67,1,9,67,71,2,71,10,75,1,75,6,79,2,10,79,83,1,5,83,87,2,87,10,91,1,91,5,95,1,6,95,99,2,99,13,103,1,103,6,107,1,107,5,111,2,6,111,115,1,115,13,119,1,119,2,123,1,5,123,0,99,2,0,14,0"
 
+import math 
+
 def form(st):
     linp = str.split(st,",")
     for i in range(0, len(linp)): 
@@ -10,8 +12,9 @@ def form(st):
 
 linp = form(inp)
 
-
-for i in range(ncodes):
+def thing(li):
+    ncodes = math.ceil(len(li)/4)
+    for i in range(ncodes):
         #print(i)
         instructpos = 4*i
         #print(instructpos)
@@ -26,11 +29,12 @@ for i in range(ncodes):
             li[li[instructpos+3]] = li[li[instructpos+1]]*li[li[instructpos+2]]
         else: 
             return "no correct instruction"
-    return li        
+    return li
+    
 #print(thing(linp))
 
 
-#tests
+#p1 tests
 t1 = "1,9,10,3,2,3,11,0,99,30,40,50"
 assert(thing(form(t1))==form("3500,9,10,70,2,3,11,0,99,30,40,50"))
 t2 = "1,0,0,0,99" #2,0,0,0,99
@@ -42,8 +46,26 @@ assert(thing(form(t4))==form("2,4,4,5,99,9801"))
 t5 = "1,1,1,4,99,5,6,0,99" #30,1,1,4,2,5,6,0,99
 assert(thing(form(t5))==form("30,1,1,4,2,5,6,0,99"))
 
-print(linp)
-linp[1]=12
-linp[2]=2
-print(linp)
-print(thing(linp))
+
+#apparently part 2 wants the output to be list[0], after giving explicit tests in part 1 for the output to be a list, so, here's a function
+def stupid(lis):
+    return lis[0]
+
+def adjust(lis,a,b):    # a is "noun" and b is "verb" despite the fact that the instruction should be the verb
+    lis[1] = a 
+    lis[2] = b
+    return lis 
+    
+#def findoutput(lis,a,b,out):
+#    for n in range(0,100):
+#        break
+#    return lis
+    
+#print range(0,100)
+    
+#p1 show your work
+#print(linp)
+#linp[1]=12
+#linp[2]=2
+#print(linp)
+#print(thing(linp))
