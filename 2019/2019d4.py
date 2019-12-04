536a878021a0aba342791d2993ea96c689cea27e
Meghan <meghanleighgreen@gmail.com>
2019-12-04 06:32:52 +0000

    zoom.

--- 2019/2019d4.py
+++ 2019/2019d4.py
@@ -12,17 +12,26 @@ def valid(rr):
         test1 = str(maybe)[0] == str(maybe)[1] or str(maybe)[1] == str(maybe)[2] or str(maybe)[2] == str(maybe)[3] or str(maybe)[3] == str(maybe)[4] or str(maybe)[4] == str(maybe)[5]
         if test1:
             test2 = int(str(maybe)[0]) <= int(str(maybe)[1]) and int(str(maybe)[1]) <= int(str(maybe)[2]) and int(str(maybe)[2]) <= int(str(maybe)[3]) and int(str(maybe)[3]) <= int(str(maybe)[4]) and int(str(maybe)[4]) <= int(str(maybe)[5])
-                fair.append(maybe)
+            unit = 0
+            if test2:
+                for d in str(maybe):
+                    if str(maybe).count(d) == 2:
+                        unit+=1
+                if unit>0:
+                    fair.append(maybe)
     return len(fair)
 
 
-#print(valid(passrange))
+print(valid(passrange))
 
 
+#tblob = "123445"
+#for i in tblob:
+#    print(tblob.count(i))
 
 #s = []
 maybe = 111111
-print(str(maybe).count("1"))
+#print(str(maybe).count("1"))
 #print(str(maybe)[0] == str(maybe)[1] or str(maybe)[1] == str(maybe)[2] or str(maybe)[2] == str(maybe)[3] or str(maybe)[3] == str(maybe)[4] or str(maybe)[4] == str(maybe)[5])
 #print(int(str(maybe)[0]) <= int(str(maybe)[1]) and int(str(maybe)[1]) <= int(str(maybe)[2]) and int(str(maybe)[2]) <= int(str(maybe)[3]) and int(str(maybe)[3]) <= int(str(maybe)[4]) and int(str(maybe)[4]) <= int(str(maybe)[5]))
 #s.append(maybe)
@@ -30,4 +39,8 @@ print(str(maybe).count("1"))
 #print(int(str(maybe)[0]))
 
 # part 1, assert won't be valid after part 2 edits
-assert(valid(passrange)==2814)
+# assert(valid(passrange)==2814)
+
+#part 2 is not 1678
+# part 2, 
+# assert(valid(passrange)==1991)
