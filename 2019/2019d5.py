54f446402aa2fc677559b4899271ec2a9e896e1d
Meghan <meghanleighgreen@gmail.com>
2019-12-05 21:53:26 +0000

    added debugging mode

--- 2019/2019d5.py
+++ 2019/2019d5.py
@@ -34,7 +34,7 @@ def heaviside(a,b,c): #a = 0 or 1
 #print(heaviside(1,"a","b")) #b 
 #print(heaviside(0,"c","d")) #c
 
-def thing3(li): # copied from day 2
+def thing3(li,d=0): # copied from day 2
     li = form(li)
     i = 0
     while i < len(li):
@@ -42,18 +42,20 @@ def thing3(li): # copied from day 2
         if p[3] == 99:
             break
         elif p[3] == 1:
-            print("intcode is ",(li[i:i+4]))
-            print("pieces are",p,str(p[3])+" means add")
-            print(li[li[i+1]],li[li[i+2]])
+            if d: 
+                print("intcode is ",(li[i:i+4]))
+                print("pieces are",p,str(p[3])+" means add")
+                print(li[li[i+1]],li[li[i+2]])
             li[li[i+3]] = heaviside(p[2],li[li[i+1]],li[i+1]) + heaviside(p[1],li[li[i+2]],li[i+2]) 
-            print("intcode is now ",(li[i:i+4]),"\n")
+            if d: print("intcode is now ",(li[i:i+4]),"\n")
             i += 4
         elif p[3] == 2:
-            print("intcode is ",(li[i:i+4]))
-            print("pieces are",p,str(p[3])+" means multiply")
-            print(li[li[i+1]],li[li[i+2]])
+            if d: 
+                print("intcode is ",(li[i:i+4]))
+                print("pieces are",p,str(p[3])+" means multiply")
+                print(li[li[i+1]],li[li[i+2]])
             li[li[i+3]] = heaviside(p[2],li[li[i+1]],li[i+1]) * heaviside(p[1],li[li[i+2]],li[i+2])
-            print("intcode is now ",(li[i:i+4]),"\n")
+            if d: print("intcode is now ",(li[i:i+4]),"\n")
             i += 4
         elif p[3] == 3:
             print("pieces are",p,str(p[3])+" means store new input")
@@ -65,7 +67,7 @@ def thing3(li): # copied from day 2
             return str(p[3])+" means no correct instruction"
     return li[0]
 
-
+#day 2 tests
 t0="1,9,10,3,2,3,11,0,99,30,40,50" #3500
 assert(thing3(t0)==3500)
 t1="1,0,0,0,99" #2 
@@ -76,9 +78,8 @@ t3="2,4,4,5,99,0" #2
 assert(thing3(t3)==2)
 t4="1,1,1,4,99,5,6,0,99" #30
 assert(thing3(t4)==30)
-
-d2test = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,9,23,27,1,6,27,31,1,31,9,35,2,35,10,39,1,5,39,43,2,43,9,47,1,5,47,51,1,51,5,55,1,55,9,59,2,59,13,63,1,63,9,67,1,9,67,71,2,71,10,75,1,75,6,79,2,10,79,83,1,5,83,87,2,87,10,91,1,91,5,95,1,6,95,99,2,99,13,103,1,103,6,107,1,107,5,111,2,6,111,115,1,115,13,119,1,119,2,123,1,5,123,0,99,2,0,14,0"
-    
+d2test = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,9,23,27,1,6,27,31,1,31,9,35,2,35,10,39,1,5,39,43,2,43,9,47,1,5,47,51,1,51,5,55,1,55,9,59,2,59,13,63,1,63,9,67,1,9,67,71,2,71,10,75,1,75,6,79,2,10,79,83,1,5,83,87,2,87,10,91,1,91,5,95,1,6,95,99,2,99,13,103,1,103,6,107,1,107,5,111,2,6,111,115,1,115,13,119,1,119,2,123,1,5,123,0,99,2,0,14,0"
+assert(thing3(d2test)==3101844)
     
 #print(thing3(inp))
 
