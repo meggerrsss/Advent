2a93b26344981bb21339ec7a0bbbd6d67a5cd045
Meghan <meghanleighgreen@gmail.com>
2019-12-09 02:38:13 +0000

    enjoyed bringing back proper file input, numpy, and list splitting

--- 2019/2019d8.py
+++ 2019/2019d8.py
@@ -2,16 +2,11 @@ import numpy
 numbs = [int(i) for i in open('input.txt', 'r').read().strip()]
 wide,tall=25,6
 
-# god i hope those dimensions are right, previewing at this scale is hard.
-# wait i have a small one to test on lol
-
 def mat(ar,w,t):
   wide,tall = w,t
   nlayers = int(len(ar)/(wide*tall))
   matrix = numpy.reshape(ar,(nlayers,tall,wide))
   return matrix
-
-
 
 t00 = [int(i) for i in "123456789012".strip()]
 wide0,tall0 = 3 ,2
@@ -27,10 +22,29 @@ def count(matr):
     d[layer][3] = numpy.count_nonzero(matr[layer] == 2)
   return d
 
-
 a = count(mat(numbs,wide,tall))
 sorted = a[a[:,1].argsort()] # sort by number of zeroes
 row = sorted[0] # row with fewest zeroes
-ans = int(row[2])*int(row[3])
-print(ans)
+ans = int(row[2])*int(row[3]) # number of 1s in least-zero-line multiplied by number of 2s in least-zero-line
 
+
+# p2
+
+def flatten(matr):
+  nlayers,tall,wide = len(matr),len(matr[0]),len(matr[0][0])
+  flat = numpy.zeros((tall,wide)) +2
+  for layer in range(nlayers):
+    for t in range(tall):
+      for w in range(wide):
+        if flat[t][w]==2:
+          #print("w =",w,"t =",t,"l =",layer,"v =",flat[t][w])
+          flat[t][w] = matr[layer][t][w]
+  return flat
+
+t1 = [int(i) for i in "0222112222120000".strip()]
+wide1,tall1 = 2,2
+mat1 = mat(t1,wide1,tall1)
+#print(mat1)
+
+print(flatten(mat(numbs,wide,tall)))
+#and then i used excel to display it because python wasn't letting me output something wide enough to visually process it
