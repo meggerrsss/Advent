ee8f6903930c20729336381cd480629c1fe98455
Meghan <meghanleighgreen@gmail.com>
2019-12-09 01:48:09 +0000

    last few lines wouldn't go so i just. printed and multiplied by hand.

--- 2019/2019d8.py
+++ 2019/2019d8.py
@@ -6,31 +6,31 @@ wide,tall=25,6
 # wait i have a small one to test on lol
 
 def mat(ar,w,t):
-  wide = w
-  tall = t
+  wide,tall = w,t
   nlayers = int(len(ar)/(wide*tall))
   matrix = numpy.reshape(ar,(nlayers,tall,wide))
   return matrix
 
-#print(mat(numbs,wide,tall)[0][0])
+
 
 t00 = [int(i) for i in "123456789012".strip()]
 wide0,tall0 = 3 ,2
 mat0 = mat(t00,wide0,tall0)
-#print(mat0)
 
 def count(matr):
   nlayers,tall,wide = len(matr),len(matr[0]),len(matr[0][0])
-  d = numpy.zeros((nlayers,3))
-  print(nlayers,tall,wide)
+  d = numpy.zeros((nlayers,4))
   for layer in range(len(matr)):
-    d[layer][0] = numpy.count_nonzero(matr[layer] == 0)
-    d[layer][1] = numpy.count_nonzero(matr[layer] == 1)
-    d[layer][2] = numpy.count_nonzero(matr[layer] == 2)
+    d[layer][0] = layer #original layer number
+    d[layer][1] = numpy.count_nonzero(matr[layer] == 0)
+    d[layer][2] = numpy.count_nonzero(matr[layer] == 1)
+    d[layer][3] = numpy.count_nonzero(matr[layer] == 2)
   return d
-#print(numpy.zeros((100,3)))
 
 
-print(count(mat0))
-#def matrix(s,width,height):
-#    nlayers - len(s)/(wide*tall)
+a = count(mat(numbs,wide,tall))
+sorted = a[a[:,1].argsort()] # sort by number of zeroes
+row = sorted[0] # row with fewest zeroes
+ans = int(row[2])*int(row[3])
+print(ans)
+
