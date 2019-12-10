a964939866d9e39fe759c0b1931596366de3b0be
Meghan <meghanleighgreen@gmail.com>
2019-12-10 01:02:19 +0000

    kinda satisfying finding a problem in my code 4 years later

--- 2015/day18.py
+++ 2015/day18.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import animation
 
-f = open('day18.txt', 'r')
+f = open('input.txt', 'r')
 inp = f.read()
 f.close()
 
 
 def blankgrid(n, m): # n is number of rows, m is number of columns, from day 6
-    grid = [[0 for j in xrange(m)] for i in xrange(n)]
+    grid = [[0 for j in range(m)] for i in range(n)]
     return grid
 
 
@@ -44,26 +44,26 @@ def count(mat): # from day 6
     return tally
 
 
-def neighbours(grid,lat,long):
+def neighbours(grid,lat,long): # given a grid and a position in the grid, returns what state the neighbours are in
     size = (len(grid),len(grid[0]))
-    if lat == 0 and long == 0: #corners
+    if lat == 0 and long == 0: #corners, top left
         l = [grid[lat+1][long], grid[lat+1][long+1], grid[lat][long+1]]
-    elif lat == 0 and long == size[1]-1:
+    elif lat == 0 and long == size[1]-1: #corners, top right?
         l = [grid[lat+1][long], grid[lat+1][long-1], grid[lat][long-1]]
-    elif lat == size[0]-1 and long == size[1]-1:
+    elif lat == size[0]-1 and long == size[1]-1: #corners, bottom right
         l = [grid[lat-1][long], grid[lat-1][long-1], grid[lat][long-1]]
-    elif lat == size[0]-1 and long == 0:
+    elif lat == size[0]-1 and long == 0: #corners, bottom left?
         l = [grid[lat-1][long], grid[lat-1][long+1], grid[lat][long+1]]
-    elif lat == 0: #edges
+    elif lat == 0: #edges, top?
         l = [grid[lat+1][long], grid[lat+1][long-1], grid[lat+1][long+1], grid[lat][long-1], grid[lat][long+1]]
-    elif lat == size[0]-1:
+    elif lat == size[0]-1: #edges, bottom?
         l = [grid[lat-1][long], grid[lat-1][long-1], grid[lat-1][long+1], grid[lat][long-1], grid[lat][long+1]]
-    elif long == 0:
+    elif long == 0: #edges, left? 
         l = [grid[lat][long+1], grid[lat-1][long+1], grid[lat+1][long+1], grid[lat-1][long], grid[lat+1][long]]
-    elif long == size[1]-1:
+    elif long == size[1]-1: #edges, right?
         l = [grid[lat][long-1], grid[lat-1][long-1], grid[lat+1][long-1], grid[lat-1][long], grid[lat+1][long]]
     else: #centre
-        l = [grid[lat+1][long+1], grid[lat][long+1], grid[lat+1][long], grid[lat-1][long+1], grid[lat+1][long-1], grid[lat-1][long], grid[lat-1][long], grid[lat-1][long-1]]
+        l = [grid[lat+1][long+1], grid[lat][long+1], grid[lat+1][long], grid[lat-1][long+1], grid[lat+1][long-1], grid[lat-1][long], grid[lat][long-1], grid[lat-1][long-1]]
     return l
 
 
@@ -102,17 +102,17 @@ def iterswitch(grid,n):
     return grid
 
 
-grid = numgrid(initgrid(inp,100,100))
-print neighbourson(grid,10,10)[1]
-print count(iterswitch(grid,100))
+#grid = numgrid(initgrid(inp,100,100))
+#print neighbourson(grid,10,10)[1]
+#print count(iterswitch(grid,100))
 
 #print "Day 18: \nPart 1: {0} \nPart 2: {1}".format("1","2")
 
 
-grid = np.array(numgrid(initgrid(inp,100,100)))
-x = range(100)
-y = range(100)
-x, y = np.meshgrid(x, y)
-plt.pcolormesh(x, y, grid)
-plt.colorbar()
-plt.show()
+#grid = np.array(numgrid(initgrid(inp,100,100)))
+#x = range(100)
+#y = range(100)
+#x, y = np.meshgrid(x, y)
+#plt.pcolormesh(x, y, grid)
+#plt.colorbar()
+#plt.show()
