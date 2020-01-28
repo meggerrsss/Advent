1500b392655a589c10196c2e37fc24c97f02d054
Meghan <meghanleighgreen@gmail.com>
2020-01-28 01:58:59 +0000

    me realizing there are letter pairs in the rules list

--- 2015/day19.py
+++ 2015/day19.py
@@ -3,12 +3,12 @@
 inp = "CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl"
 
 rules = [line[:-1].split(" ") for line in open('rules.txt')]
-print(rules)
+#print(rules)
 
 def mutate(st,r):
   #lett = list(st)
   lett = st
-  n = [] #mutetated string
+  #n = [] #mutetated string
   s = set() #set 
   for i in range(len(lett)):
     for j in range(len(r)):
@@ -16,11 +16,16 @@ def mutate(st,r):
       if r[j][0] == lett[i]:
         n = str(lett[0:i])+str(r[j][2])+str(lett[i+1:])
         s.add(n)
+      if r[j][0] == lett[i:i+2]:
+        n = str(lett[0:i])+str(r[j][2])+str(lett[i+1:])
+        s.add(n)
   return len(s)
 
+print(mutate(inp,rules))
 
 
-testrules = [line[:-1].split(" ") for line in open('testrules.txt')]
-print(mutate("HOH",testrules))
 
 
+
+#testrules = [line[:-1].split(" ") for line in open('testrules.txt')]
+#print(mutate("HOH",testrules))
