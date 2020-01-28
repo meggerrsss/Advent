80e615835a3d4a8c262300187f476315ee5f8d0c
Meghan <meghanleighgreen@gmail.com>
2020-01-28 01:51:48 +0000

    back at it

---
+++ 2015/day19.py
@@ -0,0 +1,26 @@
+# 2015 day 19
+
+inp = "CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl"
+
+rules = [line[:-1].split(" ") for line in open('rules.txt')]
+print(rules)
+
+def mutate(st,r):
+  #lett = list(st)
+  lett = st
+  n = [] #mutetated string
+  s = set() #set 
+  for i in range(len(lett)):
+    for j in range(len(r)):
+      #print(r[j][0])
+      if r[j][0] == lett[i]:
+        n = str(lett[0:i])+str(r[j][2])+str(lett[i+1:])
+        s.add(n)
+  return len(s)
+
+
+
+testrules = [line[:-1].split(" ") for line in open('testrules.txt')]
+print(mutate("HOH",testrules))
+
+
