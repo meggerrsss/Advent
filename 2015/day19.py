0bc2a223cdbf29e407b9c4e247cb8b1a71bfcc78
Meghan <meghanleighgreen@gmail.com>
2020-01-28 02:16:49 +0000

    iterative version works on part 1 inputs

--- 2015/day19.py
+++ 2015/day19.py
@@ -5,27 +5,46 @@ inp = "CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYP
 rules = [line[:-1].split(" ") for line in open('rules.txt')]
 #print(rules)
 
-def mutate(st,r):
+
+def mutate(st,r,itera=1,s=set()): #it doesn't wipe clean if run twice in the same file 
   #lett = list(st)
   lett = st
-  #n = [] #mutetated string
-  s = set() #set 
-  for i in range(len(lett)):
-    for j in range(len(r)):
-      #print(r[j][0])
-      if r[j][0] == lett[i]:
-        n = str(lett[0:i])+str(r[j][2])+str(lett[i+1:])
-        s.add(n)
-      if r[j][0] == lett[i:i+2]:
-        n = str(lett[0:i])+str(r[j][2])+str(lett[i+2:])
-        s.add(n)
-  return len(s)
+  if itera == 0:
+    return s
+  else:
+    for i in range(len(lett)):
+      for j in range(len(r)):
+        #print(r[j][0])
+        if r[j][0] == lett[i]:
+          n = str(lett[:i])+str(r[j][2])+str(lett[i+1:])
+          s.add(n)
+        if r[j][0] == lett[i:i+2]:
+          n = str(lett[:i])+str(r[j][2])+str(lett[i+2:])
+          s.add(n) 
+    return mutate(st,r,itera-1,s)
 
-print(mutate(inp,rules))
+print(len(mutate(inp,rules)))
+testrules = [line[:-1].split(" ") for line in open('testrules.txt')]
+print(len(mutate("HOH",testrules)))
+
+#iterate?
 
 
 
 
 
-#testrules = [line[:-1].split(" ") for line in open('testrules.txt')]
-#print(mutate("HOH",testrules))
+#reverse?
+startt = "e"
+
+def unmutate(st,r):
+  lett = st
+  s = set()
+
+
+#print(unmutate("HOH",testrules))
+
+
+
+
+
+
