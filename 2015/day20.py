2de1ba2a2ca130ed26f8246aa903be9849d817c6
Meghan <meghanleighgreen@gmail.com>
2020-01-29 00:27:46 +0000

    cutting down number of iterations to find factors in factors()

--- 2015/day20.py
+++ 2015/day20.py
@@ -1,22 +1,32 @@
 # day 20
 
+import math as m
+
 inp = 34000000
 
 #from https://www.programiz.com/python-programming/examples/factor-number
 def factors(x): 
   l = []
-  for i in range(1, x + 1):
+  for i in range(1, int((x+2)/2)):
     if x % i == 0:
       l.append(i)
+  l.append(x)
   return l
 
 def presents(n):
   return sum(factors(n))*10
 
-for i in range(100000):
-  if presents(i) == inp:
+for i in range(10):
+  if presents(i) >= inp:
     print(i)
     break
+
+for i in range(10):
+  print(presents(i))
+
+
+#i probably should do this in reverse  
+#print(inp/10)
 
 
 # [10,30,40,70,60,120,80,150,130]
