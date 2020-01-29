f70a31a85b348023cf4482a5a32d52ee3cea5e97
Meghan <meghanleighgreen@gmail.com>
2020-01-29 00:37:49 +0000

    prime factors might be needed maybe?

--- 2015/day20.py
+++ 2015/day20.py
@@ -4,14 +4,42 @@ import math as m
 
 inp = 34000000
 
-#from https://www.programiz.com/python-programming/examples/factor-number
-def factors(x): 
+# from https://www.programiz.com/python-programming/examples/factor-number 
+# with edits from https://www.geeksforgeeks.org/sum-factors-number/
+def factors(x): # n -> [list of any factors of n, including 1 and itself]
   l = []
   for i in range(1, int((x+2)/2)):
     if x % i == 0:
       l.append(i)
   l.append(x)
   return l
+
+#from https://www.geeksforgeeks.org/python-program-to-check-whether-a-number-is-prime-or-not/
+def isprime(n): # n -> whether or not n is prime
+  if n<=1:
+    return False
+  if n<=3:
+    return True
+  if (n % 2 == 0 or n % 3 == 0):
+    return False
+  i = 5
+  while(i * i <= n):
+    if (n % i == 0 or n % (i + 2) == 0):
+      return False
+    i = i + 6
+  return True
+
+print(isprime(7))
+
+def primefactors(x): # n -> [list of prime factors of n]
+  f = factors(x)
+  pf = []
+  for i in f:
+    if isprime(i):
+      pf.append(i)
+  return pf
+
+print(primefactors(353))
 
 def presents(n):
   return sum(factors(n))*10
@@ -21,8 +49,8 @@ for i in range(10):
     print(i)
     break
 
-for i in range(10):
-  print(presents(i))
+#for i in range(10):
+#  print(presents(i))
 
 
 #i probably should do this in reverse  
