b77e59340ad93b36a9c19cc8043767fcedf8a5d7
Meghan <meghanleighgreen@gmail.com>
2020-12-02 08:51:16 +0000

    i actually like the string ones?

---
+++ 2020/2020d2.py
@@ -0,0 +1,19 @@
+f = open('2020/inputs/2020d2input.txt', 'r')
+inp = f.read()
+f.close() 
+
+linp = inp.split('\n')
+
+def valid(passex): #a single test
+  s = passex.split(' ') #just splitted the sections
+  rang = [int(x) for x in s[0].split("-")]   #proud of this
+  req = s[1][0]    
+  passe = s[2]
+  t = passe.count(req)
+  return t in range(rang[0],rang[1]+1)
+
+
+
+
+
+print(valid(linp[0]))
