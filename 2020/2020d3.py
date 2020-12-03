a5ad3ca942452ab37eb00899729500056fb18007
Meghan <meghanleighgreen@gmail.com>
2020-12-03 07:04:24 +0000

    functional enough!

--- 2020/2020d3.py
+++ 2020/2020d3.py
@@ -36,19 +36,27 @@ def convert(vec):
       v[s] = 1
   return v
 
-def pathe(matr,mx,my,star=[0,0]):
-  v = list(matr[0][0])
+def pathe(matr,mx,my,star=[0,0]):   #mx>0 for right, my>0 for down
+  v = list(matr[star[0]][star[1]])  #still need to generalize 
   pos = star
   lenx = len(matr[0])
   leny = len(matr)
   for j in range(math.ceil(leny/my)):
     pos = [(pos[0]+mx) % lenx,(pos[1]+my) % leny]
-    print(pos)
     v.append(matr[pos[1]][pos[0]])
-  return v
+  return sum(convert(v))
 
+#part one answer
 print(pathe(linp,3,1))
 
-#number of trees hit (tree = #)
-print(convert(pathe(linp,3,1)))
+#part two
+testslopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+def trymany(matr,tes):
+  s = 1
+  for x in range(len(tes)):
+    v = pathe(matr,tes[x][0],tes[x][1])
+    s = s*v
+  return s
 
+#part two answer
+print(trymany(linp,testslopes))
