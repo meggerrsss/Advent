6f226ae9d94e64a8be4173c1333feda9fc00654c
Meghan <meghanleighgreen@gmail.com>
2020-12-03 06:33:31 +0000

    desperately avoiding modulo arithmetic

--- 2020/2020d3.py
+++ 2020/2020d3.py
@@ -1,18 +1,29 @@
 f = open('2020/inputs/2020d3input.txt', 'r')
 inp = f.read()
 f.close() 
-
 linp = inp.split('\n')
 
-print(linp)
 
 def extend(matr):
   lenx = len(matr[0])
   leny = len(matr)
-  while lenx<leny:
+  while lenx<=leny:
     matr = [row+row for row in matr]
     lenx = len(matr[0])
     leny = len(matr)
+    print(matr)
   return matr
 
 print(extend(linp))
+
+def pathe(matr,mx,my):
+  pos = (0,0)
+  leny = len(matr)
+  v = [matr[0][0]]
+  print(v)
+  for j in range(leny-1):
+    pos = (pos[0]+mx, pos[1]+my)
+    v.append(matr[pos[1]][pos[0]])
+  return v 
+
+#print(pathe(extend(linp),1,3))
