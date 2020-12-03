8b571ee2fac50bdfa44d29829ffd577d57da947a
Meghan <meghanleighgreen@gmail.com>
2020-12-03 07:05:32 +0000

    cleaning and hopefully generalizing, did not test .. negatives

--- 2020/2020d3.py
+++ 2020/2020d3.py
@@ -5,30 +5,6 @@ linp = inp.split('\n')
 
 import math 
 
-def extend(matr,x=1):
-  lenx = len(matr[0])
-  leny = len(matr)
-  while lenx<=leny:
-    matr = [row+row for row in matr]
-    lenx = len(matr[0])
-    leny = len(matr)
-    print(matr)
-  return matr
-
-#print(extend(linp))
-
-def pathe1(matr,mx,my):
-  pos = (0,0)
-  leny = len(matr)
-  v = [matr[0][0]]
-  print(v)
-  for j in range(leny-1):
-    pos = (pos[0]+mx, pos[1]+my)
-    v.append(matr[pos[1]][pos[0]])
-  return v 
-
-#inxt = extend(linp)
-
 def convert(vec):
   v = [0] * len(vec)
   for s in range(len(vec)):
@@ -37,7 +13,7 @@ def convert(vec):
   return v
 
 def pathe(matr,mx,my,star=[0,0]):   #mx>0 for right, my>0 for down
-  v = list(matr[star[0]][star[1]])  #still need to generalize 
+  v = list(matr[star[0]][star[1]]) 
   pos = star
   lenx = len(matr[0])
   leny = len(matr)
