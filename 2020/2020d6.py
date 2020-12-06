a72fe55f1c029ff5ba92735287cfb2f4a35d3c3a
Meghan <meghanleighgreen@gmail.com>
2020-12-06 09:34:31 +0000

    i should have seen that coming

--- 2020/2020d6.py
+++ 2020/2020d6.py
@@ -3,19 +3,30 @@ inp = f.read()
 f.close() 
 linp = inp.split('\n\n')
 
-print(linp)
 
-def toset(li):
+def tosetpart1(li):
   setted = [0] * len(li)
   for x in range(len(li)):
     setted[x] = set(li[x].replace('\n',''))
   return setted
 
-print(toset(linp))
+
+def tosetpart2(li):
+  setted = [0] * len(li)
+  for x in range(len(li)):
+    splitfirst = li[x].split('\n')
+    splitfirst = [set(t) for t in splitfirst]
+    setted[x] = set(splitfirst[0])
+    for y in splitfirst:
+      setted[x] = setted[x].intersection(y)
+    numpeep = li[x].count('\n')+1
+  return setted
+
+
+print(tosetpart2(linp))
 
 def sumsetlengths(los): # los = list of sets, output from toset
   v = [len(x) for x in los]
-  print(v)
   return sum(v)
 
-print(sumsetlengths(toset(linp)))
+print(sumsetlengths(tosetpart2(linp)))
