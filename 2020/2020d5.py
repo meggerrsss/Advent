6f3ac13f98ad86ce9e8cae118029f52845454720
Meghan <meghanleighgreen@gmail.com>
2020-12-06 00:40:08 +0000

    something is Wrong

--- 2020/2020d5.py
+++ 2020/2020d5.py
@@ -1,40 +1,73 @@
 f = open('2020/inputs/2020d5input.txt', 'r')
 inp = f.read()
 f.close() 
+linp = inp.split('\n')
 
 
 example = "FBFBBFFRLR"
 
-def rown(st):   #st is FBFBBFFRLR
+def rown(st,deb = False):   #st is FBFBBFFRLR
   st = st[0:6]
   rang = range(0,128)
-  print(rang)
   while len(st) > 0:
     currentletter = st[0]
     if st == "F":
       return min(rang)
     if st == "B":
-      return max(rang)
+      return max(rang)-1
     if st == "":
       return rang
     if currentletter == "F":
       spread = int((max(rang)-min(rang)+1)/2)
-      print(spread)
+      newlow = min(rang)
+      newhigh = max(rang) - spread
+      rang = range(newlow,newhigh+1)
+      if deb: print("spread: ",spread,", range: ", rang, ", letter: ",currentletter, " remaining: ", st[1:])
+      st = st[1:]
+    if currentletter == "B":
+      spread = int((max(rang)-min(rang)+1)/2)
+      newlow = min(rang) + spread
+      newhigh = max(rang)
+      rang = range(newlow,newhigh+1)
+      if deb: print("spread: ",spread,", range: ", rang, ", letter: ",currentletter, " remaining: ", st[1:])
+      st = st[1:]
+  return rang
+
+def seatn(st):   #st is FBFBBFFRLR
+  st = st[7:]
+  rang = range(0,8)
+  while len(st) > 0:
+    currentletter = st[0]
+    if st == "L":
+      return min(rang)
+    if st == "R":
+      return max(rang)
+    if st == "":
+      return rang
+    if currentletter == "L":
+      spread = int((max(rang)-min(rang)+1)/2)
       newlow = min(rang)
       newhigh = max(rang) - spread
       rang = range(newlow,newhigh+1)
       st = st[1:]
-      print(st)
-      print(rang)
-    if currentletter == "B":
+    if currentletter == "R":
       spread = int((max(rang)-min(rang)+1)/2)
-      print(spread)
       newlow = min(rang) + spread
       newhigh = max(rang)
       rang = range(newlow,newhigh+1)
       st = st[1:]
-      print(st)
-      print(rang)
   return rang
 
-print(rown(example))
+def seatid(st):
+  row = rown(st)
+  seat = seatn(st)
+  return row * 8 + seat 
+
+
+print(linp)
+
+saved = 0 
+for person in linp:
+  s = seatid(person)
+  if s > saved: saved = s
+print(saved)
