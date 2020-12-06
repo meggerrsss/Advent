b3502f6144732a44246bc0e0518508fc3b2f6ad6
Meghan <meghanleighgreen@gmail.com>
2020-12-06 00:56:37 +0000

    added debugging but still have bugs

--- 2020/2020d5.py
+++ 2020/2020d5.py
@@ -7,13 +7,16 @@ linp = inp.split('\n')
 example = "FBFBBFFRLR"
 
 def rown(st,deb = False):   #st is FBFBBFFRLR
-  st = st[0:6]
+  st = st[0:7]
+  if deb: print("starting sequence: ",st )
   rang = range(0,128)
   while len(st) > 0:
     currentletter = st[0]
     if st == "F":
+      if deb: print("letter: ",st,", range: ", rang, ", final: ", min(rang))
       return min(rang)
     if st == "B":
+      if deb: print("letter: ",st,", range: ", rang, ", final: ", max(rang)-1)
       return max(rang)-1
     if st == "":
       return rang
@@ -22,25 +25,28 @@ def rown(st,deb = False):   #st is FBFBBFFRLR
       newlow = min(rang)
       newhigh = max(rang) - spread
       rang = range(newlow,newhigh+1)
-      if deb: print("spread: ",spread,", range: ", rang, ", letter: ",currentletter, " remaining: ", st[1:])
+      if deb: print("letter: ",currentletter,", range: ", rang, ", spread: ",spread, " remaining: ", st[1:])
       st = st[1:]
     if currentletter == "B":
       spread = int((max(rang)-min(rang)+1)/2)
       newlow = min(rang) + spread
       newhigh = max(rang)
       rang = range(newlow,newhigh+1)
-      if deb: print("spread: ",spread,", range: ", rang, ", letter: ",currentletter, " remaining: ", st[1:])
+      if deb: print("letter: ",currentletter,", range: ", rang, ", spread: ",spread, " remaining: ", st[1:])
       st = st[1:]
   return rang
 
-def seatn(st):   #st is FBFBBFFRLR
+def seatn(st, deb = False):   #st is FBFBBFFRLR
   st = st[7:]
+  if deb: print("starting sequence: ",st )
   rang = range(0,8)
   while len(st) > 0:
     currentletter = st[0]
     if st == "L":
+      if deb: print("letter: ",st,", range: ", rang, ", final: ", min(rang))
       return min(rang)
     if st == "R":
+      if deb: print("letter: ",st,", range: ", rang, ", final: ", max(rang))
       return max(rang)
     if st == "":
       return rang
@@ -49,25 +55,27 @@ def seatn(st):   #st is FBFBBFFRLR
       newlow = min(rang)
       newhigh = max(rang) - spread
       rang = range(newlow,newhigh+1)
+      if deb: print("letter: ",currentletter,", range: ", rang, ", spread: ",spread, " remaining: ", st[1:])
       st = st[1:]
     if currentletter == "R":
       spread = int((max(rang)-min(rang)+1)/2)
       newlow = min(rang) + spread
       newhigh = max(rang)
       rang = range(newlow,newhigh+1)
+      if deb: print("letter: ",currentletter,", range: ", rang, ", spread: ",spread, " remaining: ", st[1:])
       st = st[1:]
   return rang
 
-def seatid(st):
-  row = rown(st)
-  seat = seatn(st)
-  print(row,seat)
+def seatid(st, deb = False):
+  row = rown(st, deb)
+  seat = seatn(st,deb)
+  if deb: print(row,seat)
   return row * 8 + seat 
 
-print(seatid("BFFFBBFRRR"))
+print(seatid("BBFFBBFRLL",True))
 
-#saved = 0 
-#for person in linp:
-#  s = seatid(person)
-#  if s > saved: saved = s
-#print(saved)
+saved = 0 
+for person in linp:
+  s = seatid(person)
+  if s > saved: saved = s
+print(saved)
